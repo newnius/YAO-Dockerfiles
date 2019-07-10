f79ab58e5d8db240086d59cd02b5859c83f2e41e
Newnius <newnius.cn@gmail.com>
2019-07-10 20:47:02 +0800

    update dependencies

--- base/bootstrap.sh
+++ base/bootstrap.sh
@@ -12,6 +12,11 @@ if [ -d /workspace ]; then
   cd /workspace
 fi
 
+if ! grep -q *.py per_process_gpu_memory_fraction; then
+  echo "Resourse usage limitation not set!"
+  exit 1
+fi
+
 # use eval because commands likes `key=value command` would cause file not found error when using $@, but this eval will ruin current environment
 eval $@
 
