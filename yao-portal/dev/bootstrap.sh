610ee98efbcb316a8a103f615257b63e8087e96f
Newnius <newnius.cn@gmail.com>
2019-12-04 18:52:14 +0800

    update yao-portal

--- yao-portal/dev/bootstrap.sh
+++ yao-portal/dev/bootstrap.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 if [[ -d /config/ ]]; then
-	if [[ ! -f /config/config.js ]]; then
-		cp /var/www/html/static/config.js /config/config.js
-	fi
-
 	if [[ ! -f /config/config.inc.php ]]; then
 		cp /var/www/html/config.inc.php /config/config.inc.php
 	fi
-fi
-
-if [[ -f /config/config.js ]]; then
-	rm /var/www/html/static/config.js
-	ln -s /config/config.js /var/www/html/static/config.js
 fi
 
 if [[ -f /config/config.inc.php ]]; then
